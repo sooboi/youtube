57a9feb3070ff39f101053d59724ac73dcbe38ad
sooboi <dlgbrkd123@gmail.com>
2023-09-04 00:55:37 +0900

    수정 : Header 스타일 tailwind 사용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,24 +23,22 @@ export default function Header() {
   }, [keyword]);
 
   return (
-    <header>
-      <Link to="/">
-        <div>
-          <IoLogoYoutube /> <h1>Youtube</h1>
-        </div>
+    <header className="w-full flex p-4 text-2xl border-b border-zinc-600 mb-4">
+      <Link to="/" className="flex items-center">
+        <IoLogoYoutube className="text-4xl text-brand" />{" "}
+        <h1 className="font-bold ml-2 text-3xl">Youtube</h1>
       </Link>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            placeholder="Search ..."
-            value={text}
-            onChange={handleChange}
-          />
-          <button>
-            <CiSearch />
-          </button>
-        </div>
+      <form onSubmit={handleSubmit} className="w-full flex justify-center ">
+        <input
+          className="w-7/12 p-2 outline-none bg-black text-gray-50"
+          type="text"
+          placeholder="Search ..."
+          value={text}
+          onChange={handleChange}
+        />
+        <button className="bg-zinc-600 px-4">
+          <CiSearch />
+        </button>
       </form>
     </header>
   );
