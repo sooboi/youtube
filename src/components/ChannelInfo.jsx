2fc4264d0fd2cf4866475ebc2e9c9879794cda6b
sooboi <dlgbrkd123@gmail.com>
2023-09-07 01:49:01 +0900

    수정 : 비디오 상세 스타일링 및 사이드 영상추가

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ChannelInfo({ id, name }) {
-  return <div>{name}</div>;
+  const { youtube } = useYoutubeApi();
+
+  const { data: url } = useQuery(["channel", id], () =>
+    youtube.channelImageURL(id)
+  );
+
+  return (
+    <div className="flex my-4 mb-8 items-center">
+      {url && <img src={url} alt={name} className="w-10 h-10 rounded-full" />}
+      <p className="text-lg font-medium ml-2">{name}</p>;
+    </div>
+  );
 }
--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "./VideoCard";
 
 export default function RelatedVideos({ id }) {
-  return <div>{id}</div>;
+  const { youtube } = useYoutubeApi();
+
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["related", id], () => youtube.relatedVideos(id));
+
+  return (
+    <>
+      {isLoading && <p>Loading . . .</p>}
+      {error && <p>Something is wrong !</p>}
+      {videos && (
+        <ul>
+          {videos.map((it) => (
+            <VideoCard key={it.id} video={it} type="list" />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
